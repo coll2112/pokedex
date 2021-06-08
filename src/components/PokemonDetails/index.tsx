6815a41dd893b36e92f623d5260e1b212322e50a
coll2112 <lgc9312@gmail.com>
2021-06-08 02:22:13 -0500

    restyled some components

--- src/components/PokemonDetails/index.tsx
+++ src/components/PokemonDetails/index.tsx
@@ -20,22 +20,32 @@ const PokemonDetails = ({ params }) => {
     return <Error />
   }
 
-  const pokeTypes = data.types.map((t) => (
-    <p key={t.type.name} className={styles.typeName}>
-      {t.type.name}
-    </p>
-  ))
-
   // converts from hectograms to pounds
   const convertWeight = (data.weight / 4.536).toFixed(2)
   // converts from decimetres to feet
   const convertHeight = (data.height / 3.048).toFixed(1).replace('.', "'")
 
-  const switchImg = (
+  const pokeTypes = data.types.map((t) => (
+    <p key={t.type.name} className={styles.type}>
+      {t.type.name}
+    </p>
+  ))
+
+  const frontViewSprite = (
     <Image
       alt={`Sprite of ${data.name}`}
+      className={styles.sprite}
       height={200}
       src={toggleImg ? data.sprites.front_shiny : data.sprites.front_default}
+      width={200}
+    />
+  )
+  const backViewSprite = (
+    <Image
+      alt={`Sprite of ${data.name}`}
+      className={styles.sprite}
+      height={200}
+      src={toggleImg ? data.sprites.back_shiny : data.sprites.back_default}
       width={200}
     />
   )
@@ -47,7 +57,11 @@ const PokemonDetails = ({ params }) => {
           <h3 className={styles.name}>{data.name}</h3>
           <h3 className={styles.number}>{pokemonIdPrefix(data.id)}</h3>
         </div>
-        <div className={styles.imgContainer}>{switchImg}</div>
+        <div className={styles.types}>{pokeTypes}</div>
+        <div className={styles.imgContainer}>
+          {backViewSprite}
+          {frontViewSprite}
+        </div>
       </div>
       <div className={styles.btnContainer}>
         <button
@@ -55,17 +69,13 @@ const PokemonDetails = ({ params }) => {
           type="button"
           onClick={() => setToggleImg(!toggleImg)}
         >
-          Show {toggleImg ? 'Default' : 'Shiny'} Variation
+          Show {toggleImg ? 'Default' : 'Shiny'} Version
         </button>
         <Link href="/">
           <button className={styles.button} type="button">
             Back to Results
           </button>
         </Link>
-      </div>
-      <div className={styles.statContainer}>
-        <p className={styles.statTitle}>Types:</p>
-        <div className={styles.types}>{pokeTypes}</div>
       </div>
       <div className={styles.statContainer}>
         <p className={styles.statTitle}>Weight:</p>
@@ -75,7 +85,7 @@ const PokemonDetails = ({ params }) => {
       </div>
       <div className={styles.statContainer}>
         <p className={styles.statTitle}>Height: </p>
-        <p className={styles.stat}>{convertHeight}</p>
+        <p className={styles.stat}>{convertHeight}"</p>
       </div>
     </div>
   )
